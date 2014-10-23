bf96e002e3460c3e0088ee73d4f6e70b279ed882
baloi <baloi.galia@gmail.com>
2014-10-22 20:02:15 -0400

    final version of strlen_test.c

--- kernigan/strlen_test.c
+++ kernigan/strlen_test.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 
-#define MAXLEN  1000
+#define MAXLINE  1000
 
 int str_len(char s[]);
+int get_line(char s[], int limit);
 
 int main(int argc, char **argv)
 {
-    int length = 0; 
+    int length = 0, i; 
     //char s[] = "hello";
-    char s[] = "hello";
-    char line[MAXLEN];
+    char line[MAXLINE];
 
-    length = str_len(s);
+    while( (i = get_line(line, MAXLINE)) > 0) {
+        length = str_len(line);
 
-    printf("%s is %d characters long", s, length);
+        printf("%s is %d characters long", line, length);
+
+    }
 
     return 0;
 
+}
+
+int get_line(char s[], int limit)
+{
+    int c, i;
+
+    for (i = 0; i < limit && (c=getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    // end of line?
+    s[i] = '\0';
+
+    return i;
 }
 
 int str_len(char s[])
