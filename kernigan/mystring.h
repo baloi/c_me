f4568cbc96c548e4814a5b1a851aae9b7e9b0c3b
baloi <baloi.galia@gmail.com>
2014-11-26 08:10:28 -0500

    mystring

---
+++ kernigan/mystring.h
@@ -0,0 +1,8 @@
+/* copy 'to' from 'from'; assume it is big enough */
+void copy(char to[], char from[]) {
+    int i = 0;
+
+    while ((to[i] = from[i]) != '\0') {
+        i++;
+    }
+}
---
+++ kernigan/test_mystring.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mystring.h"
+
+int main(int argc, char **argv) {
+    char orig[] = "dada";
+    char *to = malloc(sizeof(orig));
+    return 0;
+}
