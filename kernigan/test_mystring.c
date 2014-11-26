bc41758573cc4d55755d9918b341c3090c45f7e4
baloi <baloi_galia@yahoo.com>
2014-11-26 13:08:07 -0500

    test_mystring

--- kernigan/test_mystring.c
+++ kernigan/test_mystring.c
@@ -2,12 +2,19 @@
 #include <stdlib.h>
 #include "mystring.h"
 
+void test_copy();
+
 int main(int argc, char **argv) {
+    test_copy();
+    return 0;
+}
+
+void test_copy() {
     char orig[] = "dada";
     char *to = malloc(sizeof(orig));
 
     copy(to, orig);
     
     printf("copied >>%s<< to variable to\n", to);
-    return 0;
+
 }
