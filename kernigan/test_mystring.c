bea90533c79a851b7998a6474f380789d41845ce
baloi <baloi_galia@yahoo.com>
2014-11-26 13:06:54 -0500

    test_mystring

--- kernigan/test_mystring.c
+++ kernigan/test_mystring.c
@@ -5,5 +5,9 @@
 int main(int argc, char **argv) {
     char orig[] = "dada";
     char *to = malloc(sizeof(orig));
+
+    copy(to, orig);
+    
+    printf("copied >>%s<< to variable to\n", to);
     return 0;
 }
